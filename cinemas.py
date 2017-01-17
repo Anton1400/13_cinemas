be1b418e59ae3f97a6a3c0d5bdfa7f1a66324281
Anton1400 <akkulov.anton@gmail.com>
2017-01-17 22:24:41 +0400

    Update cinemas.py

--- cinemas.py
+++ cinemas.py
@@ -2,7 +2,7 @@ import requests
 import beautifulsoup4
 
 def fetch_afisha_page():
-    pass
+    return requests.get('http://www.afisha.ru/msk/schedule_cinema/').text
 
 
 def parse_afisha_list(raw_html):
@@ -14,12 +14,13 @@ def fetch_movie_info(movie_title):
 
 
 def output_movies_to_console(movies):
-    print(|название|оценка|etc|) # Напечатать в виде таблиы
-    print(|--------|------|---|)
+    print('|название|оценка|etc|') # Напечатать в виде таблиы
+    print('|--------|------|---|')
     for cinema in movies:
         code
         print('*'*70)
 
 
 if __name__ == '__main__':
-    pass
+    movies = parse_afisha_list(fetch_afisha_page()) # html страница афиши, которую нужно распарсить, информация содержиться в псевдо таблице на сайте афиши 
+    
